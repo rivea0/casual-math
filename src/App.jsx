3c4cc12109dc5d6ae8b3932708feb9109f278b61
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-03-27 01:45:24 +0300

    Create footer component, add some styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Options from './components/Options';
 import Text from './components/Text';
 import Calculator from './components/Calculator';
+import Footer from './components/Footer';
 import './App.css';
 
 export default function App() {
@@ -16,7 +17,7 @@ export default function App() {
           <Calculator />
         </div>
       </div>
-
+      <Footer />
     </div>
   )
 }
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,7 +1,15 @@
 export default function Text() {
     return (
         <div className="text">
+            <form action="">
+                <input
+                    type="text"
+                    placeholder="Type equation..."
+                    name="equation"
+                    className="text-input"
+                />
 
+            </form>
         </div>
     )
 }
