370e275d415f205977c90038fca395f2a0843aca
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-03-27 16:54:30 +0300

    Create form

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,15 +1,16 @@
 export default function Text() {
     return (
-        <div className="text">
-            <form action="">
-                <input
-                    type="text"
-                    placeholder="Type equation..."
-                    name="equation"
-                    className="text-input"
-                />
-
-            </form>
-        </div>
+        <form action="">
+            <div className="text">
+                <label htmlFor="equation"></label>
+                    <input
+                        type="text"
+                        placeholder="Type equation..."
+                        name="equation"
+                        className="text-input"
+                        id="equation"
+                    />
+            </div>
+        </form>
     )
 }
