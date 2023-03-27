c7935b63eeec2035d7816ac21b34bfa9779b2f17
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-03-27 22:41:52 +0300

    Add `onClick` event handler, use `id` for `Button` components

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,45 +1,46 @@
 import 'katex/dist/katex.min.css';
 import { InlineMath } from 'react-katex';
 
-export default function Text() {
-    return (
-        <div className="calculator">
-            <Button text={<InlineMath math="\sqrt{}"/>} role="role-big" />
-            <Button text="C" role="role-big2" />
-            <Button text={<InlineMath math="x^2"/>} role="role-small" />
-            <Button text={<InlineMath math="x^y"/>} role="role-small2" />
-            <Button text="π" role="role-small3" />
-            <Button text="e" role="role-small4" />
-            <Button text="sin" role="role-small5" />
-            <Button text="cos" role="role-small6" />
-            <Button text="tan" role="role-small7" />
-            <Button text="ln" role="role-small8" />
-            <Button text="asin" role="role-small9" />
-            <Button text="acos" role="role-small10" />
-            <Button text="atan" role="role-small11" />
-            <Button text={<InlineMath math="log_{10}"/>} role="role-small12" />
-            <Button text="7" role="role-small13" />
-            <Button text="8" role="role-small14" />
-            <Button text="9" role="role-small15" />
-            <Button text="4" role="role-small16" />
-            <Button text="5" role="role-small17" />
-            <Button text="6" role="role-small18" />
-            <Button text="1" role="role-small19" />
-            <Button text="2" role="role-small20" />
-            <Button text="3" role="role-small21" />
-            <Button text="⌫" role="role-small22" />
-            <Button text="0" role="role-small23" />
-            <Button text="." role="role-small24" />
-            <Button text="⏎" role="role-small25" />
-        </div>
-    )
+export default function Calculator() {
+
+  return (
+    <div className="calculator">
+      <Button text={<InlineMath math="\sqrt{}"/>} id="sqrt" />
+      <Button text="AC" id="all-clear" />
+      <Button text="x²" id="x-squared" />
+      <Button text="xⁿ" id="exp" />
+      <Button text="π" id="pi" />
+      <Button text="e" id="e" />
+      <Button text="sin" id="sin" />
+      <Button text="cos" id="cos" />
+      <Button text="tan" id="tan" />
+      <Button text="ln" id="ln" />
+      <Button text="asin" id="asin" />
+      <Button text="acos" id="acos" />
+      <Button text="atan" id="atan" />
+      <Button text="log" id="log" />
+      <Button text="7" id="seven" />
+      <Button text="8" id="eight" />
+      <Button text="9" id="nine" />
+      <Button text="4" id="four" />
+      <Button text="5" id="five" />
+      <Button text="6" id="six" />
+      <Button text="1" id="one" />
+      <Button text="2" id="two" />
+      <Button text="3" id="three" />
+      <Button text="⌫" id="backspace" />
+      <Button text="0" id="zero" />
+      <Button text="." id="dot" />
+      <Button text="⏎" id="enter" />
+      <Button text="+" id="plus" />
+      <Button text="-" id="minus" />
+      <Button text="×" id="multiply" />
+      <Button text="÷" id="divide" />
+    </div>
+  )
 }
 
 
-function Button(props) {
-    return (
-        <div className={props.role}>
-            {props.text}
-        </div>
-    )
+function Button({ id, text }) {
+  return <div id={id} onClick={(e) => console.log(e.target)}>{text}</div>;
 }
