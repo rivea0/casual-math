d3fb734d5fc9f75822211c6d8eae8c115699e791
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-03-28 20:51:47 +0300

    Add id to option elements

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,22 +1,3 @@
-// export default function Options(props) {
-
-//     return (
-//         <div id="options">
-//             <Option 
-//                 name="Simplify" 
-//                 onClick={props.simplify}
-//             />
-//             <Option name="Factor" />
-//             <Option name="Derive" />
-//             <Option name="Integrate" />
-//             <Option name="Find 0's" />
-//             <Option name="Find Tangent Line" />
-//             <Option name="Find Area Under Curve" />
-
-//         </div>
-//     )
-// }
-
-export default function Option({ name, onClick }) {
-    return <div className="option" onClick={onClick}>{name}</div>;
+export default function Option({ name, id, onClick }) {
+    return <div className="option" id={id} onClick={onClick}>{name}</div>;
 }
