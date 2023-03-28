98c169e5c6e7a8e4704e3a55b766c58fef12f6fa
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-03-28 20:53:14 +0300

    Add handler for change events to update value, add autofocus

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,30 +1,29 @@
-export default function Text({ innerText }) {
+export default function Text({ innerText, textValue, handleChange }) {
   return (
-    (innerText.length > 1) ?
+    (innerText.length > 1 && Array.isArray(innerText)) ?
     <div className="text">
         {innerText.map(textItem => <p key={textItem}>{textItem}</p>)}
     </div>
     :
-    <EquationForm placeholder={innerText[0]} />
+    <EquationForm placeholder={innerText[0]} textValue={textValue} handleChange={handleChange}/>
   )
 }
 
 
-function EquationForm({ placeholder }) {
+function EquationForm({ textValue, handleChange }) {
   return (
-    <form
-      action=""
-      onSubmit={e => {
-        e.preventDefault();}}
-    >
+    <form action="" onSubmit={e => { e.preventDefault(); }}>
       <div className="text-form">
         <label htmlFor="equation"></label>
           <input
               type="text"
-              placeholder={placeholder}
+              placeholder={textValue}
               name="equation"
               className="text-input"
               id="equation"
+              ref={input => input && input.focus()}
+              value={textValue}
+              onChange={handleChange}
           />
       </div>
     </form>
