2f219e0e4aa30160cdd2b015fff93eb23a038596
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-03-28 20:51:06 +0300

    Handle click events of some elements

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,16 +1,16 @@
 import 'katex/dist/katex.min.css';
 import { InlineMath } from 'react-katex';
 
-export default function Calculator() {
 
+export default function Calculator({ handleClick }) {
   return (
     <div className="calculator">
-      <Button text={<InlineMath math="\sqrt{}"/>} id="sqrt" />
-      <Button text="AC" id="all-clear" />
+      <Button text={<InlineMath math="\sqrt{}"/>} id="sqrt"  />
+      <Button text="AC" id="all-clear" handleClick={handleClick} />
       <Button text="x²" id="x-squared" />
       <Button text="xⁿ" id="exp" />
-      <Button text="π" id="pi" />
-      <Button text="e" id="e" />
+      <Button text="π" id="pi" handleClick={handleClick} />
+      <Button text="e" id="e" handleClick={handleClick} />
       <Button text="sin" id="sin" />
       <Button text="cos" id="cos" />
       <Button text="tan" id="tan" />
@@ -19,28 +19,28 @@ export default function Calculator() {
       <Button text="acos" id="acos" />
       <Button text="atan" id="atan" />
       <Button text="log" id="log" />
-      <Button text="7" id="seven" />
-      <Button text="8" id="eight" />
-      <Button text="9" id="nine" />
-      <Button text="4" id="four" />
-      <Button text="5" id="five" />
-      <Button text="6" id="six" />
-      <Button text="1" id="one" />
-      <Button text="2" id="two" />
-      <Button text="3" id="three" />
-      <Button text="⌫" id="backspace" />
-      <Button text="0" id="zero" />
-      <Button text="." id="dot" />
+      <Button text="7" id="7" handleClick={handleClick} />
+      <Button text="8" id="8" handleClick={handleClick} />
+      <Button text="9" id="9" handleClick={handleClick} />
+      <Button text="4" id="4" handleClick={handleClick} />
+      <Button text="5" id="5" handleClick={handleClick} />
+      <Button text="6" id="6" handleClick={handleClick} />
+      <Button text="1" id="1" handleClick={handleClick} />
+      <Button text="2" id="2" handleClick={handleClick} />
+      <Button text="3" id="3" handleClick={handleClick} />
+      <Button text="⌫" id="backspace" handleClick={handleClick} />
+      <Button text="0" id="0" handleClick={handleClick} />
+      <Button text="." id="." handleClick={handleClick} />
       <Button text="⏎" id="enter" />
-      <Button text="+" id="plus" />
-      <Button text="-" id="minus" />
-      <Button text="×" id="multiply" />
-      <Button text="÷" id="divide" />
+      <Button text="+" id="+" handleClick={handleClick} />
+      <Button text="-" id="-" handleClick={handleClick} />
+      <Button text="×" id="mult" />
+      <Button text="÷" id="over" />
     </div>
   )
 }
 
 
-function Button({ id, text }) {
-  return <div id={id} onClick={(e) => console.log(e.target)}>{text}</div>;
+function Button({ id, text, handleClick }) {
+  return <div id={id} onClick={handleClick}>{text}</div>;
 }
