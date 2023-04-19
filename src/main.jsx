ea023a01a693fd6e9df175a833be41a0924feb45
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-04-19 15:36:42 +0300

    Remove react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
-import Help from './Help';
-import ErrorPage from './ErrorPage';
 import './style.css';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/help',
-    element: <Help />,
-    errorElement: <ErrorPage />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 );
