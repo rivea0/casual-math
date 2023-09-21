619598762bdf41e7107409dc46d57d2f1d7c0379
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-09-22 01:50:28 +0300

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer({ color }) {
           {' '}
 
         </a>
-        <a href="mailto:riveazero@gmail.com" target="_blank" rel="noreferrer" data-testid="mail-link">
+        <a href="mailto:edae.space@gmail.com" target="_blank" rel="noreferrer" data-testid="mail-link">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-mail" data-testid="mail-icon">
             <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
             <polyline points="22,6 12,13 2,6" />
@@ -23,7 +23,7 @@ export default function Footer({ color }) {
         {' '}
         {new Date().getFullYear()}
         {' '}
-        Eda Eren.
+        Eda Eren
       </p>
     </div>
   );
